f7b43f5b259229625924c9e7bb206278eca36711
Liang <liangtianyi2002@foxmail.com>
2021-11-15 01:59:14 +0800

    创建项目

---
+++ utils/request.js
@@ -0,0 +1,16 @@
+import config from "./config";
+export default (url, method='GET', data={}) => {
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url: config.host + url,
+            method,
+            data,
+            success: (res) => {
+                resolve(res.data);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+        })
+    })
+}
