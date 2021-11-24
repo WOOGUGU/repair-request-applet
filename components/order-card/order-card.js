b2b3cef58efd6cde9821c666c94ca253059303e5
Liang <liangtianyi2002@foxmail.com>
2021-11-25 03:30:49 +0800

    取消订单请求

--- components/order-card/order-card.js
+++ components/order-card/order-card.js
@@ -1,5 +1,12 @@
+import request from "../../utils/request";
+import getStorage from "../../utils/getStorage";
+
 Component({
     properties: {
+        orderId: {
+            type: Number,
+            value: 0
+        },
         title: {
             type: String,
             value: 'title'
@@ -25,9 +32,22 @@ Component({
             value: 'time_end'
         }
     },
+
     data: {
         status: ['审核不通过', '已取消', '待审核', '待处理', '已完成'],
         color: ['_two', '_one', 'zero', 'one', 'two']
     },
-    methods: {}
+
+    methods: {
+        cancelOrder: async function (event) {
+            let id = event.currentTarget.id;
+            let userInfo = getStorage('localUserInfo');
+            let token = userInfo.token;
+            let res = await request('/updateOrder', 'POST',
+                {
+                    token,
+                });
+        }
+    }
+
 });
