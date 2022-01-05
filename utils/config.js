74c94c8c3c2a4fb39ff78371b35c85240a139365
Liang <liangtianyi2002@foxmail.com>
2022-01-05 16:04:25 +0800

    push测试

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,8 @@ export default {
     // host: 'http://192.168.0.100:8090',
     // host: 'http://192.168.0.116:8090',
     // host: 'http://192.168.0.100:8090/repairnetwork',
-    host: 'http://192.168.0.116:8090/repairnetwork',
+    host: 'http://139.196.183.218:8090/repairnetwork',
+    // host: 'http://192.168.0.116:8090/repairnetwork',
     // host: 'http://139.196.183.218:8888',
     // host: 'http://kkkoke:8090',
 }
