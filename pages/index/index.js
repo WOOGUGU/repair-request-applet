c31ef1db03f0b5dd82c5947680f010a5732f08de
Thanx <1642070679@qq.com>
2022-01-07 23:59:49 +0800

    意见反馈&关于我们内页

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,8 +5,12 @@ Page({
     /**
      * 页面的初始数据
      */
-    data: {},
+    data: {
 
+    },
+    jump:function(){
+        wx.navigateTo({url:'../instruction/instruction'})
+    },
     /**
      * 生命周期函数--监听页面加载
      */
