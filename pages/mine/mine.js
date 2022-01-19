0fea5139043f1555829d045fe438a1027162380a
huangpinchun <2407954146@qq.com>
2022-01-19 13:18:16 +0800

    我的界面登录，意见反馈验证

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,31 +1,121 @@
 import getStorage from "../../utils/getStorage";
-
+import request from "../../utils/request";
 Page({
     data: {
+        name : "n",
+        test : 0,
         topNavBar: {
             bgColor: 'bg-gradual-blue'
         },
     },
+
+    toLogin: function () {
+        wx.setStorage({
+            key: 'location',
+            data: {
+                id: 'mine'
+            }
+        }),
+            wx.navigateTo({url:'../login/login'})
+    },
+
+    confirm: async function (options) {
+        // 权限验证
+        let userInfo = getStorage('localUserInfo');
+        // 验证失败跳转
+        if (!userInfo) {
+            // 记录跳转前页面位置
+            wx.setStorage({
+                key: 'location',
+                data: {
+                    id: 'mine'
+                }
+            });
+            wx.showModal({
+                title: '系统提示',
+                content: '您还未登录，请先登录！',
+                success: function (res) {
+                    if (res.confirm) {
+                        wx.navigateTo({
+                            url: '/pages/login/login'
+                        });
+                    } else if (res.cancel) {
+                        wx.switchTab({
+                            url: '/pages/mine/mine',
+                        });
+                    }
+                }
+            })
+            return;
+        }
+        let res = await request('/selectAllOrderOfUser', 'POST',
+            {
+                token: userInfo.token,
+            });
+        console.log(res);
+        if (res.status == "wrong_token") {
+            // 记录跳转前页面位置
+            wx.setStorage({
+                key: 'location',
+                data: {
+                    id: 'mine'
+                }
+            });
+            wx.showModal({
+                title: '系统提示',
+                content: '您的登录状态已过期，请重新登录！',
+                success: function (res) {
+                    if (res.confirm) {
+                        wx.navigateTo({
+                            url: '/pages/login/login'
+                        });
+                    } else if (res.cancel) {
+                        wx.switchTab({
+                            url: '/pages/mine/mine',
+                        });
+                    }
+                }
+            })
+            return;
+        }
+        wx.navigateTo({
+            url: '/pages/opinion/opinion'
+        });
+    },
+
+
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
-
+    onLoad() {
     },
-
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
+    onReady: function (){
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
-
+    onShow: function (){
+        let userInfo = getStorage('localUserInfo');
+        if (userInfo) {
+            this.setData({
+                    test:1,
+                    name:userInfo.username
+                }
+            )
+        }else{
+            this.setData({
+                    test:0,
+                    name:"n"
+                }
+            )
+        };
     },
+
 
     /**
      * 生命周期函数--监听页面隐藏
