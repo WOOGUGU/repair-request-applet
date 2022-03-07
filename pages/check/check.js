22a24bae8611d61978c6d83cf7daf76e2d71c220
hpc123888 <2407954146@qq.com>
2022-03-07 16:40:04 +0800

    管理员工单界面和轮播图管理界面

--- pages/check/check.js
+++ pages/check/check.js
@@ -2,15 +2,19 @@ import getStorage from "../../utils/getStorage";
 import request from "../../utils/request";
 
 Page({
-    /**
-     * 页面的初始数据
-     */
     data: {
         current: 'all',
+        listData: {},
         topNavBar: {
             bgColor: 'bg-gradual-blue'
         },
-        listData: {}
+    },
+
+    // 发起工单
+    sendOrder() {
+        wx.navigateTo({
+            url: '/pages/sendOrder/sendOrder'
+        })
     },
 
     clickNavbar: function (event) {
@@ -19,17 +23,67 @@ Page({
             current: index
         });
     },
+
     onLoad: async function () {
 
     },
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: async function () {
-        wx.showModal({
-            title: '系统提示',
-            content: res.userMsg,
-            showCancel: false,
-        })
+
+    onShow: async function (options) {
+        // 权限验证
+        let userInfo = getStorage('localUserInfo');
+        let cookie = getStorage('cookie');
+        // 验证失败跳转
+        if (!userInfo || !cookie) {
+            wx.showModal({
+                title: '系统提示',
+                content: '您还未登录，请先登录！',
+                success: function (res) {
+                    if (res.confirm) {
+                        wx.navigateTo({
+                            url: '/pages/login/login'
+                        });
+                    } else if (res.cancel) {
+                        wx.switchTab({
+                            url: '/pages/index/index',
+                        });
+                    }
+                }
+            })
+            return;
+        }
+        let res = await request('/v2/order/selectAllOrderOfUser', 'GET', {
+            cookie: cookie
+        }, {
+            username: userInfo.username
+        });
+        res = res.data;
+        if (res.code == '00000') {
+            this.setData({
+                listData: res.data
+            });
+            console.log(this.data.listData);
+        } else if (res.code == 'A0200') {
+            wx.showModal({
+                title: '系统提示',
+                content: res.userMsg,
+                success: function (res) {
+                    if (res.confirm) {
+                        wx.navigateTo({
+                            url: '/pages/login/login'
+                        });
+                    } else if (res.cancel) {
+                        wx.switchTab({
+                            url: '/pages/index/index',
+                        });
+                    }
+                }
+            });
+        } else if (res.code == 'E0100') {
+            wx.showModal({
+                title: '系统提示',
+                content: res.userMsg,
+                showCancel: false,
+            });
+        }
     }
 });
