229c5bd49259a2a49a2ad3d96b3b2f8628231ae9
Liang <liangtianyi2002@foxmail.com>
2022-03-19 19:23:28 +0800

    首页轮播完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,13 @@
 // pages/index/index.js
 
+import request from "../../utils/request";
+
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {},
+    data: {
+        slideData: {}
+    },
+
     jump_1: function () {
         wx.navigateTo({url: '../instruction/instruction'})
     },
@@ -13,59 +15,18 @@ Page({
     jump_2: function () {
         wx.navigateTo({url: '../FAQ/FAQ'})
     },
-    /**
-     * 生命周期函数--监听页面加载
-     */
+
     onLoad: async function (options) {
-
+        let slideRes = await request('/v2/slide/selectAllSlide', 'GET');
+        if (slideRes.data.code == '00000') {
+            this.setData({
+                slideData: slideRes.data.data
+            });
+            console.log(this.data.slideData);
+        }
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
     onShow: async function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
 
     }
 })
