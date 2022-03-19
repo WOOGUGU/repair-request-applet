7e3d1e2b0869cbc586230bd096a68ea482d11b30
Liang <liangtianyi2002@foxmail.com>
2022-03-19 20:19:24 +0800

    请求使用https

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,3 @@
 export default {
-    // host: 'http://localhost:8080',
-    // host: 'http://localhost:8090',
-    // host: 'http://192.168.0.100:8090',
-    // host: 'http://192.168.0.116:8090',
-    host: 'http://baoxiu.liangtianyi.com',
-    // host: 'http://192.168.1.104:8090',
-    // host: 'http://192.168.0.100:8090',
-    // host: 'http://192.168.0.116:8090/repairnetwork',
-    // host: 'http://139.196.183.218:8888',
-    // host: 'http://kkkoke:8090',
+    host: 'https://baoxiu.lty923.com'
 }
